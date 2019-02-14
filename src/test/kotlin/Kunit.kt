5a5d22eb4204cf2108c1dac7a8f0a792d1927b3e
inigodm <inigo.delgado@gmail.com>
2019-02-15 00:30:19 +0100

    Avance

--- src/test/kotlin/Kunit.kt
+++ src/test/kotlin/Kunit.kt
@@ -3,8 +3,9 @@ import org.junit.Assert
 fun main(args : Array<String>) {
     var test = TestTestCase("testMethod")
     test.setUp()
-    test.testSetUp()
     test.test()
+    test.testFailedResult()
+    test.testTestResult()
 }
 
 class TestTestCase(var methodName: String){
@@ -14,36 +15,72 @@ class TestTestCase(var methodName: String){
         test = TestCaseRun(methodName)
     }
 
-    fun testSetUp(){
-        test.setup()
-        Assert.assertTrue(test.log == "setup ")
+    fun test(){
+        val result = test.run()
+        println(test.log)
+        Assert.assertTrue(test.log == "setup method executed $methodName teardown")
+        Assert.assertTrue(result.summary() == "1 test run, 0 failed")
     }
 
-    fun test(){
-        test.run()
-        Assert.assertTrue(test.wasRun)
+    fun testTestResult(){
+        var result = TestResult()
+        result.testStarted()
+        result.testFailed()
+        Assert.assertTrue(result.summary() == "1 test run, 1 failed")
+    }
+
+    fun testFailedResult(){
+        test = TestCaseRun("fakemethod")
+        val result = test.run()
+        Assert.assertTrue(result.summary() == "1 test run, 1 failed")
     }
 }
 
 open class TestCase(var methodName: String){
-    var wasRun: Boolean = false;
     var log: String = ""
+    var result = TestResult()
 
-    fun run(){
+    fun run(): TestResult{
+        result.testStarted()
         setup()
-        val cls = this.javaClass
-        cls.getMethod(methodName).invoke(this)
+        try {
+            val cls = this.javaClass
+            cls.getMethod(methodName).invoke(this)
+            log += "$methodName "
+        } catch (e: Exception) {
+            result.testFailed()
+        }
+        tearDown()
+        return result
     }
 
     fun setup(){
-        wasRun = true;
-        log += "setup "
+        log = "setup "
+    }
+
+    fun tearDown(){
+        log += "teardown"
+    }
+
+}
+
+class TestResult(var runCount: Int = 0, var failCount: Int = 0){
+    fun testStarted(){
+        runCount++;
+    }
+
+    fun testFailed(){
+        failCount++;
+    }
+
+    fun summary(): String{
+        return "$runCount test run, $failCount failed"
     }
 }
 
 class TestCaseRun(methodName: String): TestCase(methodName) {
     fun testMethod(){
-        log += "testMethod "
+        log += "method executed "
     }
 }
 
